7a71331102cdc892d5b332c082b8c6835235ebb0
LegionDev370 <llegion630@gmail.com>
2025-05-23 17:00:12 +0500

    ssasasasssssss

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello World");
 });
 
-server.listen(4000, () => {
+server.listen(4000, "0.0.0.0", () => {
   console.log("Server run port 4000");
 });
